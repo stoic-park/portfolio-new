b088b6ffa5c6eda03874c74b266eabcd611c0f2e
stoic-park <demige79@gmail.com>
2020-05-20 18:21:46 +0900

    Fix: 소개 멘트 수정
    - 소개 멘트 수정
    - 소개 멘트 어렵다

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -18,11 +18,38 @@ function About(props) {
           {/* <div className="gif-name">{mation.photo.name}</div> */}
         </div>
         <div className="About-intro">
-          {mation.intro.map((int, id) => (
-            <li className="intro-detail" key={id}>
-              {int}
-            </li>
-          ))}
+          <p className="intro-detail">
+            <div className="intro-detail">
+              사람들 틈에서 살아있음을 느낍니다
+            </div>
+            <div className="intro-detail">
+              혼자서 대단한 일을 해내는 것 보다도 사람들과 함께
+            </div>
+            <div className="intro-detail">
+              크기와 상관없이 명확한 결과물을 만들어 낼 때 보람을 느낍니다
+            </div>
+          </p>
+          <p className="intro-detail">
+            <div className="intro-detail">
+              살아오면서 겪은 여러 차례의 리더 경험을 통해 소통하는 능력을
+              익혔고
+            </div>
+            <div className="intro-detail">
+              그중에서도 상대방의 의견을 듣는 것으로부터 시작하는 소통의 자세가
+            </div>
+            <div className="intro-detail">
+              함께하는 사람들의 니즈를 보다 명확히 파악하고
+            </div>
+            <div className="intro-detail">
+              그에 맞춰 발빠르게 행동하는 사람으로 성장시켜주었습니다
+            </div>
+          </p>
+          <p></p>
+          {/* {mation.intro.map((int, id) => (
+              <li className="intro-detail" key={id}>
+                {int}
+              </li>
+            ))} */}
         </div>
       </div>
       <div className="About-body2">
